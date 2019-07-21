61ca4be14cf7ce5cd7efde6f7ea0a07cf8447a6e
Ben Taussig <bpt28@cornell.edu>
2019-07-21 11:21:23 -0400

    Only 1 album can be highlighted now

--- src/components/Collage/Collage.js
+++ src/components/Collage/Collage.js
@@ -10,24 +10,33 @@ export default class Collage extends Component {
         this.n_rows = props.n_rows;
         this.n_cols = props.n_cols;
         this.result_data = myData;
-        this.collage_style = { width: `${300 * this.n_cols}px` };
+        this.collage_style = { width: `${330 * 3}px` };//{ width: `${330 * this.n_cols}px` };
         this.state = {
-
+            collage: true,
+            showOverlay: false
         }
+
+        this.createCollage = this.createCollage.bind(this)
+        this.clickHandler = this.clickHandler.bind(this)
+        this.render = this.render.bind(this)
     }
 
     //need to be able to pass number of rows to collage element
     //created in order to set 
     createCollage() {
-        return this.result_data.map((d, ind) => {
-            return (<Image album_data={d} index={ind} />)
+        this.collage = this.result_data.map((d, ind) => {
+            return (<Image album_data={d} index={ind} showOverlay={this.state.showOverlay} clicked={false} />)
         })
+        return this.collage;
+    }
 
+    clickHandler() {
+        this.forceUpdate();
     }
 
     render() {
         return (
-            <div className="collage" style={this.collage_style}>
+            <div className="collage" style={this.collage_style} onClick={this.clickHandler}>
                 {this.createCollage()}
             </div>
         )
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -7,24 +7,55 @@ export default class image extends Component {
         this.handleClick = this.handleClick.bind(this);
         this.data = props.album_data;
         this.index = props.index;
+
         this.artist = this.data.artist.name
         this.album = this.data.name
         this.imageUrls = this.data.image
         this.playcount = this.data.playcount
 
         this.state = {
+            showOverlay: this.props.showOverlay,
+            clicked: this.props.clicked,
+            stop: false
+        }
+    }
 
+    componentDidUpdate(prevProps, prevState) {
+        //turn off unclicked overlays
+        if (!this.state.clicked && this.state.showOverlay === true && !prevState.clicked) {
+            this.setState(() => {
+                return { showOverlay: false }
+            })
+        }
+
+        //toggle clicked overlays
+        else if (this.state.clicked) {
+            this.setState(() => {
+                return {
+                    showOverlay: !this.state.showOverlay,
+                    clicked: false
+                }
+            })
         }
     }
 
     handleClick = () => {
-        console.log(this.artist, this.album, this.imageUrls, this.playcount)
+        // console.log(this.state.showOverlay)
+        // console.log("click!")
+        this.setState((state, props) => {
+            return {
+                clicked: true,
+                stop: false
+            };
+        }
+        )
     }
 
     render() {
+        let elementId = this.state.showOverlay ? "overlay" : ""
         return (
             <div className="imgWrapper" id={this.index}>
-                <div className="imgElement">
+                <div className="imgElement" id={elementId} onClick={this.handleClick}>
                     <div className="imgText">
                         <p>Artist: {this.artist}</p>
                         <p>Album: {this.album}</p>
