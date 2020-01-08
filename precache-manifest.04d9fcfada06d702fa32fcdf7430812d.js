f1cec6674d40933f08a1234d10588950c6c02ec7
Ben Taussig <bpt28@cornell.edu>
2020-01-08 17:14:33 -0500

    Updates

---
+++ precache-manifest.04d9fcfada06d702fa32fcdf7430812d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324598403be93a978b0277803e3441fa",
+    "url": "/last_fm_frontend/index.html"
+  },
+  {
+    "revision": "092965534c007dee97ef",
+    "url": "/last_fm_frontend/static/css/main.fb4becf7.chunk.css"
+  },
+  {
+    "revision": "5348c229256dcf302ee4",
+    "url": "/last_fm_frontend/static/js/2.d5d217b8.chunk.js"
+  },
+  {
+    "revision": "092965534c007dee97ef",
+    "url": "/last_fm_frontend/static/js/main.76e2b4ef.chunk.js"
+  },
+  {
+    "revision": "87ea2e1ae22688d1ff03",
+    "url": "/last_fm_frontend/static/js/runtime~main.dd94bb20.js"
+  }
+]);
