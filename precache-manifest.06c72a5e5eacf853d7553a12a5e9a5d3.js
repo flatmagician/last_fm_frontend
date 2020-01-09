e5396933a85d86bca28432aac083ec455de62b68
Ben Taussig <bpt28@cornell.edu>
2020-01-08 19:56:47 -0500

    Updates

--- precache-manifest.06c72a5e5eacf853d7553a12a5e9a5d3.js
+++ precache-manifest.06c72a5e5eacf853d7553a12a5e9a5d3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7aca0b35118f44d3ba5602ff3183a2c",
+    "revision": "54e69863dad0c213f59d38c3a271432c",
     "url": "/last_fm_frontend/index.html"
   },
   {
-    "revision": "3c9cc2e5e40e138bcd17",
+    "revision": "78187e2a0db131d6891a",
     "url": "/last_fm_frontend/static/css/main.7c993786.chunk.css"
   },
   {
-    "revision": "5348c229256dcf302ee4",
-    "url": "/last_fm_frontend/static/js/2.d5d217b8.chunk.js"
+    "revision": "366b18ca08a20370197c",
+    "url": "/last_fm_frontend/static/js/2.a19ff2a9.chunk.js"
   },
   {
-    "revision": "3c9cc2e5e40e138bcd17",
-    "url": "/last_fm_frontend/static/js/main.e436fbf1.chunk.js"
+    "revision": "78187e2a0db131d6891a",
+    "url": "/last_fm_frontend/static/js/main.fd0ab556.chunk.js"
   },
   {
     "revision": "87ea2e1ae22688d1ff03",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/last_fm_frontend/precache-manifest.113aea4bb247c47feda7dfffb0019158.js"
+  "/last_fm_frontend/precache-manifest.06c72a5e5eacf853d7553a12a5e9a5d3.js"
 );
 
 self.addEventListener('message', (event) => {
